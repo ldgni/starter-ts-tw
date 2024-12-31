c8d08a532ad6edbb1b8d38005b3e880e313e3b64
Luca Di Gianni <luca.dgni@gmail.com>
2024-12-31 11:48:02 +0100

    feat: add eslint-plugin-check-file for filename and folder naming conventions

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import checkFile from "eslint-plugin-check-file";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -23,6 +24,7 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "simple-import-sort": simpleImportSort,
+      "check-file": checkFile,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -32,6 +34,23 @@ export default tseslint.config(
       ],
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          // ignore the middle extensions of the filename to support filename like bable.config.js or smoke.spec.ts
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          // all folders within src (except __tests__)should be named in kebab-case
+          "src/**/!(__tests__)": "KEBAB_CASE",
+        },
+      ],
     },
   },
 );
