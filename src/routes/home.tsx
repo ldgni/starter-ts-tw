75b86d55932422f6d2f31072d49b8c005253fd92
Luca Di Gianni <luca.dgni@gmail.com>
2025-01-04 12:28:54 +0100

    chore: update placeholder image format from webp to jpg

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,4 +1,4 @@
-import placeholderImage from "@/assets/images/placeholder-img.webp";
+import placeholderImage from "@/assets/images/placeholder-img.jpg";
 
 export default function Home() {
   return (
