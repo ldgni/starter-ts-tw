9efda2421bae04205429f6f51ce47578f85b5964
Luca Di Gianni <luca.dgni@gmail.com>
2025-01-07 15:02:53 +0100

    style: remove margin from 404 Not Found heading

--- src/routes/not-found.tsx
+++ src/routes/not-found.tsx
@@ -3,7 +3,7 @@ import Root from "@/routes/root";
 export default function NotFoundRoute() {
   return (
     <Root>
-      <h1 className="mb-2 text-center">404 Not Found</h1>
+      <h1 className="text-center">404 Not Found</h1>
     </Root>
   );
 }
